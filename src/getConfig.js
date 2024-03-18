c89965dd0af4cfb3cc6c00962495b7957c076fa3
Toshi_さん <91844428+Toshi-san001@users.noreply.github.com>
2024-03-19 00:33:33 +0530

    Update getConfig.js

--- src/getConfig.js
+++ src/getConfig.js
@@ -2,12 +2,12 @@ import 'dotenv/config'
 const getConfig = () => {
     return {
         name: process.env.NAME || 'Devi',
-        session: process.env.SESSION || 'session',
-        prefix: process.env.PREFIX || '#',
+        session: process.env.SESSION || 'ZER',
+        prefix: process.env.PREFIX || '+',
         port: process.env.PORT || 3000,
         imgbb: process.env.IMGBB,
-        chatboturi: process.env.BRAINSHOP || '',
-        mongo: process.env.MONGO || '',
+        chatboturi: process.env.BRAINSHOP || 'http://api.brainshop.ai/get?bid=165801&key=1ftAuFL7Fhj21Fyp&uid=[uid]&msg=[msg]',
+        mongo: process.env.MONGO || 'mongodb+srv://chey:K77iYsuTRyJ4TgXf@cluster0.wtlbba3.mongodb.net/?retryWrites=true&w=majority',
         mods: process.env.MODS
             ? process.env.MODS.split(',').map((id) => {
                   if (id.endsWith('@s.whatsapp.net')) return id
