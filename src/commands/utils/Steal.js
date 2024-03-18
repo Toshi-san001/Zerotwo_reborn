06c94bedc9fbd20f3b3737b6bdc43564b181553b
Toshi_さん <91844428+Toshi-san001@users.noreply.github.com>
2024-03-19 00:28:20 +0530

    Update Steal.js

--- src/commands/utils/Steal.js
+++ src/commands/utils/Steal.js
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
         const [pack, title] = text.split('|')
         const sticker = new Sticker(await this.client.util.downloadMediaMessage(media), {
             pack: pack ?? `${this.client.config.name} Bot`,
-            author: title ?? 'By Debanjan',
+            author: title ?? 'By Dipeshuuu:)',
             type: StickerTypes.FULL,
             quality: 70
         })
